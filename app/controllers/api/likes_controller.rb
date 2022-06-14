aed4f073afa547bd93402b07b5650cf4bf592f68
Macarena Peche <macarena.peche@toptal.com>
2022-06-14 18:08:05 +0200

    Add Likes API specs and controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -7,8 +7,26 @@ module Api
       render json: @likes
     end
 
+    def create 
+      @like = @tweet.likes.build(likes_params)
+      if @like.save
+        render json: @like, status: :created
+      else 
+      render json: @like.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @like = Like.find(params[:id])
+      @like.destroy
+    end
+
     def get_tweet
       @tweet = Tweet.find(params[:tweet_id])
+    end
+
+    def likes_params
+      params.permit(:tweet_id, :user_id)
     end
   end
 end
---
+++ spec/requests/api/likes_spec.rb
@@ -0,0 +1,60 @@
+RSpec.describe 'Likes API' do
+  let!(:user) { User.create(name: "Macarena", handle: "mapeciris", email: "macarena@toptal.com") }
+  let!(:another_user) { User.create(name: "name", handle: "handle", email: "email@domain.com") }
+  let!(:tweet) { Tweet.create(content: "some content", user_id: user.id) }
+
+  describe "GET /api/tweets/:tweet_id/likes" do
+    subject(:result) do
+      get "/api/tweets/#{tweet.id}/likes"
+      response
+    end
+
+    it { is_expected.to have_http_status(200) }
+    specify { expect(JSON.parse(result.body)).to eq([]) }
+
+
+    context "with likes" do 
+      let!(:like) { Like.create(tweet_id: tweet.id, user_id: another_user.id) }
+
+      it 'shows the like' do
+        expect(JSON.parse(result.body)).to match([hash_including("tweet_id" => tweet.id, "user_id" => another_user.id)])
+      end
+    end
+  end 
+
+  describe "POST /api/tweets/:tweet_id/likes" do
+    subject(:result) do
+      post "/api/tweets/#{tweet.id}/likes", params: valid_params
+      response
+    end
+
+    context 'when request is valid' do
+      let(:valid_params) { { tweet_id: tweet.id, user_id: another_user.id } }
+
+      it { is_expected.to have_http_status(201) }
+
+      it 'creates a like on the tweet' do
+        expect { result }.to change { tweet.likes.count }.by(1)
+      end
+    end
+
+    context 'when request is invalid' do
+      before { post "/api/tweets/#{tweet.id}/likes", params: {} }
+      
+      it { expect(response).to have_http_status(422) }
+
+      it 'returns a failure message' do
+        expect(response.body).to include("can't be blank")
+      end
+    end    
+  end
+
+  describe 'DELETE /api/tweets/:tweet_id/likes/:id' do
+    let!(:like) { Like.create(tweet_id: tweet.id, user_id: another_user.id) }
+    let!(:like_id) { like.id }
+
+    before { delete "/api/tweets/#{tweet.id}/likes/#{like_id}" }
+
+    specify { expect(response).to have_http_status(204) }
+  end
+end
