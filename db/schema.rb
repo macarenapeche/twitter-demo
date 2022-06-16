ae03328783f4e06ef638153629ba6152259c9c35
Macarena Peche <macarena.peche@toptal.com>
2022-06-16 14:53:55 +0200

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_150211) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_104204) do
   create_table "follows", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "follower_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "follower_id"], name: "index_follows_on_user_id_and_follower_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
