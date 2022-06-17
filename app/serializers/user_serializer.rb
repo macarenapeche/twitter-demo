c165ca39333a0974abf5944395392830553bbf27
Macarena Peche <macarena.peche@toptal.com>
2022-06-17 11:32:00 +0200

    Add bio to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer 
-  attributes :id, :name, :handle, :email, :created_at, :updated_at
+  attributes :id, :name, :handle, :email, :bio, :created_at, :updated_at
 
   has_many :tweets
   has_many :followers
