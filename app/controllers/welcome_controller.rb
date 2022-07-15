82a603a3343f724e426ebcbde6dc85cd3e01ae30
Macarena Peche <macarena.peche@toptal.com>
2022-07-15 09:20:30 +0200

    Change basic HTTP auth

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: "user", password: "password", except: :index
-
   def index
   end
 
   def about
+    if user = authenticate_with_http_basic { |u, p| User.find_by(email: u)&.authenticate(p) }
+      @current_user = user
+    else
+      request_http_basic_authentication
+    end
   end
 end
